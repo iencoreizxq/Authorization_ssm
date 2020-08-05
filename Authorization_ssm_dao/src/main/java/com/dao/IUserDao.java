84af8c193fd89dafa02f248c8a1c8895e15f8671
zxq <1294333900@qq.com>
2020-08-05 17:47:57 +0800

    登录模块实现

---
+++ Authorization_ssm_dao/src/main/java/com/dao/IUserDao.java
@@ -0,0 +1,23 @@
+package com.dao;
+
+import com.domain.UserInfo;
+import org.apache.ibatis.annotations.Many;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
+public interface IUserDao {
+
+    @Select("select * from users where username = #{username}")
+    @Results({
+            @Result(id = true,property = "id",column = "id"),
+            @Result(property = "username",column = "username"),
+            @Result(property = "email",column = "email"),
+            @Result(property = "username",column = "username"),
+            @Result(property = "password",column = "password"),
+            @Result(property = "phoneNum",column = "phoneNum"),
+            @Result(property = "status",column = "status"),
+            @Result(property = "roles",column = "status",javaType = java.util.List.class,many = @Many(select = "com.dao.IRoleDao.findRoleByUserId"))
+    })
+    public UserInfo findByUsername(String username) throws Exception;
+}
